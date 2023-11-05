0dc4320c18116d2153aacb3a9a5861be3e498819
Ingo Reschke <kontakt@ingo-reschke.de>
2023-11-05 14:22:40 +0100

    Initial commit

---
+++ app/src/main/java/de/ingoreschke/increasesalarycalculator/MainActivity.kt
@@ -0,0 +1,67 @@
+package de.ingoreschke.increasesalarycalculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import de.ingoreschke.increasesalarycalculator.ui.theme.IncreaseSalaryCalculatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            IncreaseSalaryCalculatorTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                    CurrentSalary()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CurrentSalary(modifier: Modifier = Modifier) {
+    var currentSalary by remember { mutableStateOf(65000.00) }
+
+    TextField(
+        value = currentSalary.toString(),
+        onValueChange = { currentSalary = it.toDouble() },
+        label = { Text("Current Salary") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        modifier = modifier
+    )
+
+
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    IncreaseSalaryCalculatorTheme {
+        Greeting("Android")
+        CurrentSalary()
+    }
+}
