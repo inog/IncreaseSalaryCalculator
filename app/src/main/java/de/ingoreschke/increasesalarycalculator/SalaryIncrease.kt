01305c9823b94b94edad27e6878afa7a305af0fb
Ingo Reschke <kontakt@ingo-reschke.de>
2023-11-12 22:56:43 +0100

    add a slider

--- app/src/main/java/de/ingoreschke/increasesalarycalculator/SalaryIncrease.kt
+++ app/src/main/java/de/ingoreschke/increasesalarycalculator/SalaryIncrease.kt
@@ -1,7 +1,15 @@
 package de.ingoreschke.increasesalarycalculator
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -10,8 +18,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun SalaryIncrease(modifier: Modifier = Modifier, presenter: SalaryIncreasePresenter) {
@@ -19,7 +30,8 @@ fun SalaryIncrease(modifier: Modifier = Modifier, presenter: SalaryIncreasePrese
     var increasePercentage by remember { mutableStateOf(0.0) }
     var result by remember { mutableStateOf(0.0) }
 
-    Column(modifier = modifier) {
+
+    Column(modifier = modifier.padding(16.dp)) {
         TextField(
             value = currentSalary.toString(),
             onValueChange = {
@@ -30,7 +42,6 @@ fun SalaryIncrease(modifier: Modifier = Modifier, presenter: SalaryIncreasePrese
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
             modifier = modifier
         )
-
         TextField(
             value = increasePercentage.toString(),
             onValueChange = {
@@ -41,8 +52,20 @@ fun SalaryIncrease(modifier: Modifier = Modifier, presenter: SalaryIncreasePrese
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
             modifier = modifier
         )
+        Slider(
+            value = increasePercentage.toFloat(),
+            onValueChange = {
+                increasePercentage = it.toDouble()
+                result = presenter.calculateSalaryIncrease(currentSalary, increasePercentage)
+            }, modifier = modifier
+                .border(1.dp, Color.Red)
+        )
+        Spacer(modifier = modifier.height(16.dp))
+        Text(text = result.toString(),
+            fontSize = 24.sp,
+            modifier = modifier
+                .border(1.dp, MaterialTheme.colorScheme.primary ))
 
-        Text(text = result.toString(), modifier = modifier)
     }
 }
 
