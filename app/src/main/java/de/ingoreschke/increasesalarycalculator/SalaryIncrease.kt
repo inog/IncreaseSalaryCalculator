1ac351cbd5a178d1d9fcead60ef106c0885ac363
Ingo Reschke <kontakt@ingo-reschke.de>
2023-11-21 23:44:04 +0100

    move regex as constant

--- app/src/main/java/de/ingoreschke/increasesalarycalculator/SalaryIncrease.kt
+++ app/src/main/java/de/ingoreschke/increasesalarycalculator/SalaryIncrease.kt
@@ -1,13 +1,11 @@
 package de.ingoreschke.increasesalarycalculator
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -17,21 +15,21 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+val NUMBER_REGEX = "[-]?[\\d]*[.]?[\\d]*".toRegex()
+
 @Composable
 fun SalaryIncrease(modifier: Modifier = Modifier, presenter: SalaryIncreasePresenter) {
-    var currentSalary by remember { mutableStateOf(65000.00) }
+    var currentSalary by remember { mutableStateOf(0.0) }
     var increasePercentage by remember { mutableStateOf(0.0) }
     var result by remember { mutableStateOf(currentSalary) }
 
 
-    Column(modifier = modifier
-        .padding(16.dp)) {
+    Column(modifier = modifier.padding(16.dp)) {
         SalaryInput(
             value = currentSalary,
             onValueChange = {
@@ -76,34 +74,33 @@ fun SalaryIncreasePreview() {
 
 @Composable
 fun SalaryInput(modifier: Modifier = Modifier, value: Double, onValueChange: (Double) -> Unit) {
-    val numberRegex = remember { "[-]?[\\d]*[.]?[\\d]*".toRegex() }
+
     TextField(
         value = value.toString(),
         onValueChange = {
-            if (it.isEmpty() || numberRegex.matches(it)) {
+            if (it.isEmpty() || NUMBER_REGEX.matches(it)) {
                 onValueChange(it.toDouble())
             }
         },
         label = { Text("Current Salary") },
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
-        trailingIcon = { Text("€") } ,
+        trailingIcon = { Text("€") },
         modifier = modifier
     )
 }
 
 @Composable
 fun IncreaseInput(modifier: Modifier = Modifier, value: Double, onValueChange: (Double) -> Unit) {
-    val numberRegex = remember { "[-]?[\\d]*[.]?[\\d]*".toRegex() }
     TextField(
         value = value.toString(),
         onValueChange = {
-            if (it.isEmpty() || numberRegex.matches(it)){
+            if (it.isEmpty() || NUMBER_REGEX.matches(it)) {
                 onValueChange(it.toDouble())
             }
         },
         label = { Text("Increase") },
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
-        trailingIcon = { Text("%") } ,
+        trailingIcon = { Text("%") },
         modifier = modifier
     )
 }
