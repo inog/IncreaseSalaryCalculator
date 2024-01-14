f6691021f15ec14e6edd360b18730e45d8289dac
Ingo Reschke <kontakt@ingo-reschke.de>
2024-01-14 18:39:24 +0100

    try gold themes

--- app/src/main/java/de/ingoreschke/increasesalarycalculator/ui/theme/Color.kt
+++ app/src/main/java/de/ingoreschke/increasesalarycalculator/ui/theme/Color.kt
@@ -9,3 +9,12 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Gold
+val Gold80 = Color(0xFFE6C200)
+val GoldGrey80 = Color(0xFFD9C26A)
+val Orange80 = Color(0xFFE6A200)
+
+val Gold40 = Color(0xFFB38F00)
+val GoldGrey40 = Color(0xFFB39F5B)
+val Orange40 = Color(0xFFB38F00)
--- app/src/main/java/de/ingoreschke/increasesalarycalculator/ui/theme/Theme.kt
+++ app/src/main/java/de/ingoreschke/increasesalarycalculator/ui/theme/Theme.kt
@@ -16,15 +16,15 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Gold80,
+    secondary = GoldGrey80,
+    tertiary = Orange80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Gold40,
+    secondary = GoldGrey40,
+    tertiary = Orange40
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
