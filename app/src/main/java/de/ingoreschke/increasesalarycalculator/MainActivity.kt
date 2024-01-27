c449726198f8244dcea99ef3be02fa227474ee9b
Ingo Reschke <kontakt@ingo-reschke.de>
2024-01-27 15:42:13 +0100

    replace adUnitId with the one from banner. update Version

--- app/src/main/java/de/ingoreschke/increasesalarycalculator/MainActivity.kt
+++ app/src/main/java/de/ingoreschke/increasesalarycalculator/MainActivity.kt
@@ -75,7 +75,7 @@ fun AdMobBanner(modifier: Modifier = Modifier) {
         factory = { context ->
             AdView(context).apply {
                 setAdSize(AdSize.FULL_BANNER)
-                adUnitId = "ca-app-pub-1283865206002218~7018170650"
+                adUnitId = "ca-app-pub-1283865206002218/3244437360"
                 //adUnitId = "ca-app-pub-3940256099942544/6300978111" // test ad
                 loadAd(AdRequest.Builder().build())
             }
