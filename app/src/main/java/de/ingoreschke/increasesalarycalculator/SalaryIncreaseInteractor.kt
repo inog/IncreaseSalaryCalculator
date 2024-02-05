70f9435d1a8b42aea08e287db46d9c12e1d6cd3e
Ingo Reschke <kontakt@ingo-reschke.de>
2024-02-05 16:20:57 +0100

    avoid magic numbers

--- app/src/main/java/de/ingoreschke/increasesalarycalculator/SalaryIncreaseInteractor.kt
+++ app/src/main/java/de/ingoreschke/increasesalarycalculator/SalaryIncreaseInteractor.kt
@@ -1,10 +1,12 @@
 package de.ingoreschke.increasesalarycalculator
 
+private const val HUNDRED = 100.0
+
 class SalaryIncreaseInteractor {
 
     fun calcIncreasedSalary(startSalary: Double, increasePercentage: Double): Double {
-        var salaryIncrease = startSalary + (startSalary * increasePercentage / 100.0)
-        salaryIncrease = Math.round(salaryIncrease * 100.0) / 100.0
+        var salaryIncrease = startSalary + (startSalary * increasePercentage / HUNDRED)
+        salaryIncrease = Math.round(salaryIncrease * HUNDRED) / HUNDRED
         return salaryIncrease
     }
 
